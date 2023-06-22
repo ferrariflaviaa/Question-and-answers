f83b1a3cd4894e153000d957f2a40dfabe26f260
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-06-22 14:35:32 -0300

    feat: arquivos estáticos

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 //Estou dizendo o Express usar o EJS como view engine
 app.set('view engine', 'ejs');
 
+//Configuração para poder aceitar arquivos estaticos
+app.use(express.static('public'))
+
 app.get("/", (req, res) => {
   res.render("index")
 });
