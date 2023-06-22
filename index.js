b20240caffbfe62eaa517da733c6b7076a5b88ae
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-06-22 14:27:57 -0300

    feat: configurando e exibindo HTML com EJS

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+//Estou dizendo o Express usar o EJS como view engine
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+  res.render("index")
+});
+
+app.listen(8080, () => { console.log("App rodando"); })
