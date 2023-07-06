77e3531f4f1c229826a577a7420a302951c0dfc3
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-07-06 10:17:32 -0300

    fix: conexão com o banco de dados

--- database/database.js
+++ database/database.js
@@ -1,9 +1,10 @@
 const Sequelize = require('sequelize')
 
 const connection = new Sequelize('guiaperguntas', 'root', '123456', {
-  host: 'localhost',
+  host: '127.0.0.1',
   dialect: 'mysql',
-  logging: false
+  logging: false,
+  port: 3312
 })
 
 module.exports = connection;
